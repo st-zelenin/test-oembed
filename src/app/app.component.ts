7ae30f05b8524dcb914bc4bc81fe62b32b50b4ba
stepan zelenin <stepanz@google.com>
2021-07-14 15:25:54 +0200

    feat(oEmbed): added fb sdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+interface Brand {
+  name: string;
+  url: string;
+}
+
+interface OEmbedParams {
+  url: string;
+}
+interface OEmbedResponse {
+  error?: { message: string; };
+
+  height: number;
+  width: number;
+  html: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +23,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-oembed';
+  brands: Brand[] = [
+    { name: 'P&G Polska', url: 'https://www.facebook.com/PGPolska' },
+    { name: 'Nike', url: 'https://www.facebook.com/nike' },
+    { name: 'Reebok', url: 'https://www.facebook.com/ReebokPoland/' },
+    { name: 'Nestle', url: 'https://www.facebook.com/NestlePL' },
+  ];
+
+  selectedBrand: Brand | undefined = undefined;
+  embeddedContent: OEmbedResponse | undefined = undefined;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  selectBrand(brand: Brand) {
+    this.selectedBrand = brand === this.selectedBrand ? undefined : brand;
+
+    const params: OEmbedParams = { url: brand.url };
+    FB.api<OEmbedParams, OEmbedResponse>(
+      "/oembed_page",
+      params,
+      (response) => {
+        if (!response || response.error) {
+          this.embeddedContent = {
+            height: 200,
+            width: 200,
+            html: this.sanitizer.sanitize(SecurityContext.HTML, response.error?.message || '') || ''
+          }
+        } else {
+          this.embeddedContent = response;
+        }
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,58 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+function initializeApp(): Promise<void> {
+  return new Promise((resolve, reject) => {
+
+    const extendedWindow = window as unknown as any;
+    extendedWindow.fbAsyncInit = function () {
+      FB.init({
+        appId: extendedWindow.APP_ID,
+        cookie: true,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v11.0'
+      });
+
+      FB.getLoginStatus((response) => {
+        console.log('auth response', response);
+
+        if (!response.authResponse) {
+          FB.login((response) => {
+            if (response.authResponse) {
+              console.log('Welcome!  Fetching your information.... ');
+
+              FB.api('/me', (response: { name: string }) => {
+                console.log('Good to see you, ' + response.name + '.');
+              });
+
+              resolve();
+            } else {
+              console.log('User cancelled login or did not fully authorize.');
+            }
+          });
+        } else {
+          resolve();
+        }
+      });
+    };
+
+    (function (d) {
+      if (d.getElementById('facebook-jssdk')) {
+        return;
+      }
+
+      const firstScript = d.getElementsByTagName('script')[0];
+      const sdkScript = d.createElement('script');
+      sdkScript.id = 'facebook-jssdk';
+      sdkScript.src = "https://connect.facebook.net/en_US/sdk.js";
+      firstScript.parentNode!.insertBefore(sdkScript, firstScript);
+    }(document));
+  });
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +62,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: () => initializeApp,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
